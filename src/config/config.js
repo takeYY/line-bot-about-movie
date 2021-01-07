d6643f2ef5591a22b78d6416926536ae90f98abf
takeyy <t0k6yist@gmail.com>
2021-01-07 16:52:27 +0900

    URIsの修正

--- src/config/config.js
+++ src/config/config.js
@@ -9,8 +9,8 @@ exports.conf = {
     yahooAPI: process.env.YAHOO_API,
   },
   URIs: {
-    yahoo: `https://map.yahooapis.jp/search/local/V1/localSearch?appid=${keys.yahooAPI}&gc=0305001&sort=dist&output=json`,
-    tmdb: `https://api.themoviedb.org/3/discover/movie?api_key=${keys.tmdbAPI}&include_adult=false&page=1`,
+    yahoo: `https://map.yahooapis.jp/search/local/V1/localSearch?appid=${this.keys.yahooAPI}&gc=0305001&sort=dist&output=json`,
+    tmdb: `https://api.themoviedb.org/3/discover/movie?api_key=${this.keys.tmdbAPI}&include_adult=false&page=1`,
     movieLIFF: 'https://liff.line.me/1654406577-Dv0PZqRO',
     theaterLIFF: 'https://liff.line.me/1654383535-Ww5vMA9l/',
   },
